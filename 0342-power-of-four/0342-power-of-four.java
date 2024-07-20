b19a4117facee12468dd6651ea419d0ac503a64f
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2024-07-20 22:24:31 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (25.58%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0)
+            return false;
+        if(n == 1)
+            return true;
+        if ((1073741824 % n == 0) && (n % 10 == 4 || n % 10 == 6))
+            return true;
+        return false;
+    }
+}
