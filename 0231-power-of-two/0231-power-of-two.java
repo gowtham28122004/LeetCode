8a58807ca4fc337619ae372895042b3dda7f2198
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2024-09-29 12:02:07 +0530

    Time: 1 ms (42.63%), Space: 41.1 MB (18.76%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        if(n <= 0){
+            return false;
+        }
+        double t = Math.log10(n) / Math.log10(2);
+        return (t - (int)t) == 0;
     }
 }
