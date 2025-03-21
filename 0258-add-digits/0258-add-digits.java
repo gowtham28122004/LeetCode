b46ab6e1fd66aaab8ddc67f568705d79db570c6d
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2025-03-21 14:46:52 +0530

    Time: 0 ms (100%), Space: 40.6 MB (93.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,10 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        if(num == 0)
-            return 0;
-        if(num % 9 == 0)
-            return 9;
-        else 
-            return num % 9;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 }
