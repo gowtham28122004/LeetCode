99cf16fb5d077eb5706a611ab674ca3ee2167bfe
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2025-07-10 15:30:57 +0530

    Time: 16 ms (18.97%), Space: 70.8 MB (8.37%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
@@ -5,8 +5,8 @@ class Solution {
             Arrays.sort(nums[i]);
         }
         for(int i = 0;i < nums[0].length;i++) {
-            int max = Integer.MIN_VALUE;
-            for(int j = 0;j < nums.length;j++) {
+            int max = nums[0][i];
+            for(int j = 1;j < nums.length;j++) {
                 if(max < nums[j][i]) max = nums[j][i];
             }
             score += max;
